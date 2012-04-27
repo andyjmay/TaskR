e5b6755c31df344800fee31e4f85003f6a4e573e
Andy May <andyjmay@gmail.com>
2012-04-27 18:52:39 -0400

    Added windows client project skeleton

---
+++ TaskR.WindowsClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("TaskR.WindowsClient")]
+[assembly: AssemblyDescription("Simple task management demo application powered by SignalR")]
+[assembly: AssemblyVersion("0.4.0.0")]
