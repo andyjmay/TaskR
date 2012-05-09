ec734482cc1512258e25c8b1d2bcdd6dc87451ca
Andy May <andyjmay@gmail.com>
2012-05-09 09:39:10 -0400

    Updated to 0.5 final

--- TaskR/Models/Task.cs
+++ TaskR/Models/Task.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Web;
 
 namespace TaskR.Models {
   public class Task : INotifyPropertyChanged {
